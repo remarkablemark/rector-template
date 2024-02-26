1e33787803e95970f3d78d5742c79e80dbcbe80f
remarkablemark <remarkablemark@users.noreply.github.com>
2024-02-26 07:23:29 +0000

    style(php-cs-fixer): fix code style and lint

--- src/ExampleRector.php
+++ src/ExampleRector.php
@@ -15,6 +15,7 @@ final class ExampleRector extends AbstractRector
 {
     /**
      * @see https://github.com/rectorphp/php-parser-nodes-docs/
+     *
      * @return array<class-string<Node>>
      */
     public function getNodeTypes(): array
@@ -29,7 +30,7 @@ final class ExampleRector extends AbstractRector
     {
         $methodName = $this->getName($node->name);
 
-        if (! str_starts_with((string) $methodName, 'set')) {
+        if (!str_starts_with((string) $methodName, 'set')) {
             return null;
         }
 
@@ -42,7 +43,8 @@ final class ExampleRector extends AbstractRector
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
-            'Change method calls from set* to change*.', [
+            'Change method calls from set* to change*.',
+            [
                 new CodeSample(
                     // code before
                     '$user->setPassword("123456");',
